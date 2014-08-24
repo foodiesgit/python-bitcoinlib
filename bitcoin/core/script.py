1b0374010e6e69af8a5d651efccaa4edd9a09713
FoodieGIT <invinuit@gmail.com>
2014-08-24 21:52:12 +0000

    myriadcoinlib

--- bitcoin/core/script.py
+++ bitcoin/core/script.py
@@ -889,7 +889,7 @@ def RawSignatureHash(script, txTo, inIdx, hashtype):
 
     if inIdx >= len(txTo.vin):
         return (HASH_ONE, "inIdx %d out of range (%d)" % (inIdx, len(txTo.vin)))
-    txtmp = bitcoin.core.CMutableTransaction.from_tx(txTo)
+    txtmp = copy.deepcopy(txTo)
 
     for txin in txtmp.vin:
         txin.scriptSig = b''
